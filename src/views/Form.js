d4034f4ce8966f8d2056f398821b1f45d4421cca
axarisar <axelarielsaravia@gmail.com>
2022-03-22 09:08:27 -0300

    Update all

--- src/views/Form.js
+++ src/views/Form.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {Textarea, Select, Option, Input} from "./FormInputs.js"
 
 
@@ -6,8 +6,9 @@ function Cols(props) {
   const cols = props.cols
   const colsNames = Object.keys(cols);
   return (
-    <div className="form-input">
-      {colsNames.map(colName => (
+    <>
+    {colsNames.map(colName => (
+        <div className="form-input flex-c align-c justify-c">
         <React.Fragment key={colName} >
           <label className="fs-text-s fm_lb">{colName + ":"}</label>
           {cols[colName].type === "textarea"
@@ -24,6 +25,7 @@ function Cols(props) {
               placeholder={cols[colName].placeholder}
               min={cols[colName].min}
               max={cols[colName].max}
+              send={props.send}
             />
           }
           {cols[colName].required && 
@@ -43,8 +45,9 @@ function Cols(props) {
             </div>
           }
         </React.Fragment>
-      ))}
-    </div>
+      </div>
+    ))}
+    </>
   );
 }
 
@@ -53,7 +56,7 @@ function Row(props) {
     <>
       {props.rows.map((cols, i) => (
         <div key={"row-" + i} className="flex-r-change">
-          <Cols cols={cols}/>
+          <Cols cols={cols} send={props.send}/>
         </div>
       ))}
     </>
@@ -61,20 +64,26 @@ function Row(props) {
 }
 
 function FormButtons(props) {
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+  
   return (
     <div className="form-input flex-r-change align-c justify-c">
-      <button name="submit" type="submit" className="fs-text-s fm_btn">Enviar por email</button>
+      <button name="submit" onClick={handleSubmit} type="submit" className="fs-text-s fm_btn">Enviar por email</button>
       <div style={{width: "10px", height: "10px"}}/>
       <button type="button">
-        <a className="fm_btn fs-text-s w-api" href="./#" rel="noopener noreferrer">Enviar por WhatsApp</a>
-        <a className="fm_btn fs-text-s w-desk" href="./#" rel="noopener noreferrer">Enviar por WhatsApp</a>
+        <a className="fm_btn fs-text-s w-api" href={props.href} rel="noopener noreferrer">Enviar por WhatsApp</a>
+        <a className="fm_btn fs-text-s w-desk" href={props.href} rel="noopener noreferrer">Enviar por WhatsApp</a>
       </button>
     </div>
   );
 }
 
 export default function Form(props) {
+  const [send, setSend] = useState({value: false, requiredValues: []})
+  const [href, setHref] = useState("./#");
+
   const name = props.name;
   const formData = props.formData;
   const imports = formData[name].import;
@@ -83,8 +92,8 @@ export default function Form(props) {
     <form id={name} className="cotiza-form flex-column align-c">
       <div className="form-container flex-column form-sub">
         <Row rows={formData[name].rows}/>
-        {imports && <Row rows={formData[imports].rows}/>}
-        <FormButtons />
+        {imports && <Row send={send} rows={formData[imports].rows}/>}
+        <FormButtons href={href} send={send}/>
       </div>
     </form>
   );
